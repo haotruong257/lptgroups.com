f273bb1d82396a6ca7b78afd65f2e02fd9f12306
haothetruong257 <haothetruong257@gmail.com>
2025-03-23 19:33:51 +0700

    delete temp file

--- plugins/Rating/Controllers/EvaluationController.php
+++ plugins/Rating/Controllers/EvaluationController.php
@@ -21,21 +21,3 @@ class EvaluationController extends Security_Controller {
     }
 
 }
-
-// <?php
-
-// namespace Demo\Controllers;
-
-// use App\Controllers\Security_Controller;
-
-// class Demo extends Security_Controller {
-
-//     function __construct() {
-//         parent::__construct();
-//     }
-
-//     function index() {
-//         return $this->template->rander('Demo\Views\demo\index');
-//     }
-
-// }
